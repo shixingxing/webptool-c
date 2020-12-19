682d8f7013ac60409b72e52a4c58cbc4af368bbc
shixingxing <star@DESKTOP-NO34SHR@qq.com>
2020-12-19 14:53:40 +0800

    文件排序

--- webptool/MainWindow.xaml.cs
+++ webptool/MainWindow.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using Microsoft.Win32;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics.CodeAnalysis;
+using System.Text.RegularExpressions;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace webptool
 {
@@ -28,7 +19,7 @@ namespace webptool
 
         private void Select_File_Click(object sender, RoutedEventArgs e)
         {
-            
+
             OpenFileDialog ofd = new OpenFileDialog();
             ofd.InitialDirectory = Environment.CurrentDirectory;
             ofd.Title = "请选择要打开的文件";
@@ -39,7 +30,14 @@ namespace webptool
             {
                 //选择了文件
                 String[] files = ofd.FileNames;
-                AddListItem(files);
+                ImageFile[] f = new ImageFile[files.Length];
+
+                for (int i = 0; i < files.Length; i++)
+                {
+                    f[i] = new ImageFile(files[i]);
+                }
+                Array.Sort(f);
+                AddListItem(f);
 
             }
             else
@@ -55,15 +53,44 @@ namespace webptool
 
         }
 
-        private void AddListItem(String[] files)
+        private void AddListItem(ImageFile[] files)
         {
             this.file_list.Items.Clear();
-            foreach (String file in files)
+            foreach (ImageFile file in files)
             {
                 ListViewItem item = new ListViewItem();
-                item.Content = file;
+                item.Content = file.file;
+                item.Padding = new Thickness(5, 5, 5, 5);
 
                 this.file_list.Items.Add(item);
+
+            }
+        }
+
+        public class ImageFile : IComparable<ImageFile>
+        {
+
+            private static readonly Regex regex = new Regex(@"[^\d]+");
+
+            public String file;
+
+            public ImageFile(string file)
+            {
+                this.file = file;
+            }
+
+            public int CompareTo([AllowNull] ImageFile other)
+            {
+                try
+                {
+                    int a = Int32.Parse(regex.Replace(file, ""));
+                    int b = Int32.Parse(regex.Replace(other.file, ""));
+                    return a - b;
+                }
+                catch (Exception e)
+                {
+                    return 0;
+                }
 
             }
         }
